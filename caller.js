ebb1a0d4db09c335bcdbdd9e4597c9fa54bda85f
taku.omi <taku.omi@gree.co.jp>
2018-11-16 16:35:01 +0900

    update script.

--- caller.js
+++ caller.js
@@ -1,33 +1,13 @@
 "use strict";
 exports.__esModule = true;
 function caller() {
-    /**
-     * go one line back for the caller
-     * @type {string}
-     */
     var stackLine = (new Error).stack.split("\n")[2];
-    /**
-     * retrieve the file basename & positional data, after the last `/` to the `)`
-     */
-    // 
     var caller_line = stackLine.slice(stackLine.lastIndexOf('/'), stackLine.lastIndexOf(')'));
-    /**
-     *  test for no `/` ; if there is no `/` then use filename without a prefixed path
-     */
     if (caller_line.length == 0) {
         caller_line = stackLine.slice(stackLine.lastIndexOf('('), stackLine.lastIndexOf(')'));
     }
-    /**
-     * filename_base - parse out the file basename; remove first `/` char and go to `:`
-     */
     var filename_base = caller_line.slice(0 + 1, caller_line.indexOf(':'));
-    /**
-     * line_no - parse out the line number ; remove first `:` char and go to 2nd `:`
-     */
     var line_no = caller_line.slice(caller_line.indexOf(':') + 1, caller_line.lastIndexOf(':'));
-    /**
-     * line_pos - line positional - from the last `:` to the end of the string
-     */
     var line_pos = caller_line.slice(caller_line.lastIndexOf(':') + 1);
     return {
         filename_base: filename_base,
@@ -38,7 +18,14 @@ function caller() {
 exports.caller = caller;
 ;
 function position() {
-    var pos = caller();
-    return pos.filename_base + ":" + pos.line_no;
+    var stackLine = (new Error).stack.split("\n")[2];
+    var caller_line = stackLine.slice(stackLine.lastIndexOf('/'), stackLine.lastIndexOf(')'));
+    if (caller_line.length == 0) {
+        caller_line = stackLine.slice(stackLine.lastIndexOf('('), stackLine.lastIndexOf(')'));
+    }
+    var filename_base = caller_line.slice(0 + 1, caller_line.indexOf(':'));
+    var line_no = caller_line.slice(caller_line.indexOf(':') + 1, caller_line.lastIndexOf(':'));
+    var line_pos = caller_line.slice(caller_line.lastIndexOf(':') + 1);
+    return filename_base + ":" + line_no;
 }
 exports.position = position;
--- caller.ts
+++ caller.ts
@@ -1,31 +1,11 @@
 export function caller() {
-	/**
-	 * go one line back for the caller
-	 * @type {string}
-	 */
 	let stackLine = (new Error).stack.split("\n")[2];
-	/**
-	 * retrieve the file basename & positional data, after the last `/` to the `)` 
-	 */
-	// 
 	let caller_line = stackLine.slice(stackLine.lastIndexOf('/'),stackLine.lastIndexOf(')'))
-	/**
-	 *  test for no `/` ; if there is no `/` then use filename without a prefixed path
-	 */ 
 	if ( caller_line.length == 0 ) {
 		caller_line = stackLine.slice(stackLine.lastIndexOf('('),stackLine.lastIndexOf(')'))
 	}
-	/**
-	 * filename_base - parse out the file basename; remove first `/` char and go to `:`
-	 */
 	let filename_base = caller_line.slice(0+1,caller_line.indexOf(':'));
-	/**
-	 * line_no - parse out the line number ; remove first `:` char and go to 2nd `:`
-	 */
 	let line_no = caller_line.slice(caller_line.indexOf(':')+1,caller_line.lastIndexOf(':'));
-	/**
-	 * line_pos - line positional - from the last `:` to the end of the string
-	 */
 	let line_pos = caller_line.slice(caller_line.lastIndexOf(':')+1);
 
     return {
@@ -35,6 +15,14 @@ export function caller() {
     };
 };
 export function position() {
-    const pos = caller();
-    return `${pos.filename_base}:${pos.line_no}`;
+	let stackLine = (new Error).stack.split("\n")[2];
+	let caller_line = stackLine.slice(stackLine.lastIndexOf('/'),stackLine.lastIndexOf(')'))
+	if ( caller_line.length == 0 ) {
+		caller_line = stackLine.slice(stackLine.lastIndexOf('('),stackLine.lastIndexOf(')'))
+	}
+	let filename_base = caller_line.slice(0+1,caller_line.indexOf(':'));
+	let line_no = caller_line.slice(caller_line.indexOf(':')+1,caller_line.lastIndexOf(':'));
+	let line_pos = caller_line.slice(caller_line.lastIndexOf(':')+1);
+
+    return `${filename_base}:${line_no}`;
 }
